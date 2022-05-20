5432d5fe67b1535e87368f2159f06838903a0aca
rudegrass <szimmermann@uos.de>
2022-05-20 21:00:47 +0200

    fixed create_subfolder not handling path starting at root correctly

--- cloud_classifier/tools/file_handling.py
+++ cloud_classifier/tools/file_handling.py
@@ -6,17 +6,20 @@ import re
 
 def create_subfolders(path, contains_filename=True):
     if contains_filename:
-        path = os.path.split(path)[0]  # remove file nominator
+        path = os.path.split(path)[0]  # remove file from path
+    current_path = None
+    if path.startswith("/"):
+        current_path = "/"
+
     folders = path.split(os.sep)  # split path along os specific seperators
-    current_path = ""
     for fol in folders:
         if not current_path:
             current_path = fol
         else:
             current_path = os.path.join(current_path, fol)
-        if not os.path.exists(current_path):
-            if current_path:
-                os.mkdir(current_path)
+
+        if not os.path.isdir(current_path):
+            os.mkdir(current_path)
 
 
 def get_filename_pattern(structure, t_length):
